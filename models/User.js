f8efe5c334f38fdd355a33e01d91b0781afc7a6e
eyyaaaaaa <eya.chtourou1@gmail.com>
2023-03-27 16:03:41 +0200

    authpage with everything working

--- models/User.js
+++ models/User.js
@@ -131,14 +131,11 @@ UserSchema.methods.getSignedJwtToken = function () {
   });
 };
 
-UserSchema.methods.getResetPasswordToken = function () {
+UserSchema.methods.getResetPasswordToken = function () {  
   const resetToken = crypto.randomBytes(20).toString("hex");
 
   // Hash token (private key) and save to database
-  this.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
+  this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
 
   // Set token expire date
   this.resetPasswordExpire = Date.now() + 10 * (60 * 1000); // Ten Minutes
